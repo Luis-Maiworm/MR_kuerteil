21fc9a8b34f6c87b6c384210895b0ba7fdf80af2
Luis Maiworm <luis.maiworm@outlook.de>
2025-01-10 15:12:16 +0100

    added customizable background option + setting for that, cursor-camera implemented for main menu

---
+++ start_game.js
@@ -0,0 +1,23 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const startGame = document.querySelector('#startGameButton');
+    const links = document.querySelector('#links');
+    const camera = document.querySelector('[camera]');
+    const settingsWrapper = document.querySelector('#settingsWrapper');
+    console.log(camera)
+    
+    // Add event listener to the button
+    startGame.addEventListener('click', () => {
+        console.log("Start Button: Click!")
+        // Hide links
+        links.setAttribute('visible', 'false');
+        settingsWrapper.setAttribute('visible', 'false');
+        
+
+        // //rotating or let user look in correct direction? 
+        // camera.setAttribute('look-controls', 'enabled', 'false')
+        // camera.setAttribute('rotation', '0 180 0')
+
+      console.log('Links hidden and camera moved!');
+    });
+});
